c219184f8bf231e6de5fb550f777bdb4605a7247
cdhvilsted <94805251+cdhvilsted@users.noreply.github.com>
2023-03-13 15:02:44 +0100

    baseline included and attempt of t-test

---
+++ data_load_cec.py
@@ -0,0 +1,168 @@
+import os as os
+import mne
+import matplotlib.pyplot as plt
+import numpy as np 
+
+
+# The directory containing the preprocessed data
+direct = "/Users/ceciliehvilsted/Documents/GitHub/Fagpakkeprojekt2023_EEG/EEGproj-main/EEGproj-main/data_preproc"
+
+#A = audi
+#V = vis
+#tagi/tabi ord
+#kongruent vs unkongruent.
+Event_ids =  ["Tagi_A", "Tagi_A_Tabi_V", "Tagi_V", "Tabi_V", 
+              "Tagi_A_Tagi_V", "Tabi_A", "Tabi_A_Tagi_V", "Tabi_A_Tabi_V"]
+
+Aud_event = ["Tagi_A", "Tabi_A"] #only auditive
+
+Vis_event = ["Tagi_V","Tabi_V"] #only visual
+
+AV_c = ["Tagi_A_Tagi_V","Tabi_A_Tabi_V"] #congruent audivisual
+
+AV_ic = ["Tagi_A_Tabi_V","Tabi_A_Tagi_V"] # incongruent audivisual
+
+list_files = [i for i in os.listdir(direct) if i[-4:] == ".set"]
+
+
+Speech = ["PP03","PP09", "PP10", "PP11", "PP12", "PP13", "PP14", "PP15", 
+          "PP16", "PP17", "PP20", "PP25", "PP26", "PP28"]
+Non_speech = ["PP02", "PP04", "PP05", "PP06", "PP07", "PP08", "PP18", "PP19", 
+              "PP21", "PP22", "PP23", "PP24", "PP27", "PP29"]
+
+common = ['AF4', 'AFz', 'C1', 'C2', 'C3', 'C4', 'CP1', 'CP2', 'CP3', 'CP4',
+       'CP5', 'CPz', 'Cz', 'F1', 'F2', 'F3', 'F4', 'FC1', 'FC2', 'FCz',
+       'Fz', 'O1', 'O2', 'Oz', 'P1', 'P2', 'P3', 'P4', 'P5', 'P7', 'PO3',
+       'PO4', 'PO7', 'PO8', 'POz', 'Pz']
+
+Speech_files = [ i + "_4adj.set" for i in Speech]
+Non_speech_files = [i + "_4adj.set" for i in Non_speech]
+
+
+#skal ikke bruges endnu
+event_dict = { 'visual/b/high' : 1, 
+               'visual/b/mid'  : 2, 
+               'visual/b/low'  : 3,
+               'visual/g/high' : 4, 
+               'visual/g/mid'  : 5, 
+               'visual/g/low'  : 6,
+               'auditory/b/high' : 7,
+               'auditory/b/mid'  : 8, 
+               'auditory/b/low'  : 9,
+               'auditory/g/high' : 10,
+               'auditory/g/mid'  : 11, 
+               'auditory/g/low'  : 12,
+               'audiovisual/b/high/congruent': 13, 
+               'audiovisual/b/med/congruent' : 14,
+               'audiovisual/b/low/congruent' : 15,
+               'audiovisual/g/high/congruent': 16, 
+               'audiovisual/g/med/congruent' : 17,
+               'audiovisual/g/low/congruent' : 18,
+               'audiovisual/bg/high' : 19,
+               'audiovisual/bg/med'  : 20,
+               'audiovisual/bg/low'  : 21,
+               'NA1'                  : 22,   # Remove !!
+               'NA2'                  : 23,   # Remove !!
+               'NA3'                  : 24  } # Remove !!
+
+raw = mne.io.read_epochs_eeglab("/Users/ceciliehvilsted/Documents/GitHub/Fagpakkeprojekt2023_EEG/EEGproj-main/EEGproj-main/data_preproc/PP02_4adj.set", montage_units='dm')
+gennemsnit = raw.average(method='mean',by_event_type=True)
+chan = 'Cz'
+
+#for speech group
+data_As=np.zeros(141,)
+data_Vs=np.zeros(141,)
+data_AVics=np.zeros(141,)
+data_AVcs=np.zeros(141,)
+#for non-speech group
+data_Ans=np.zeros(141,)
+data_Vns=np.zeros(141,)
+data_AVicns=np.zeros(141,)
+data_AVcns=np.zeros(141,)
+
+interval = (-0.1,0)
+
+times = np.arange(-0.1,1,step=1/128)
+baseline = interval
+#raw.average().plot()
+
+#getting data from speechfiles
+# raw = epochs
+for file in Speech_files:
+    path = "/Users/ceciliehvilsted/Documents/GitHub/Fagpakkeprojekt2023_EEG/EEGproj-main/EEGproj-main/data_preproc/ " + str(file)
+    path = path.replace(" ","")
+    raw = mne.io.read_epochs_eeglab(path, montage_units='dm')
+    raw = raw.crop(tmin=-0.1) 
+    #raw = raw.apply_baseline(interval) 
+    #raw = mne.baseline.rescale(raw,times=times,baseline=(-0.1,0), mode='mean', copy=False)
+    #raw.set_channel_types({'event': 'stim'})
+    #raw.pick_types(meg=False, stim=False, eog=False, eeg=True)
+    #events = mne.events_from_annotations(raw, event_id=Event_ids)
+    #epochs = mne.Epochs(raw, events, event_id=Event_ids, baseline=None, preload=True)
+    #raw.apply_baseline(interval) 
+    #epochs.apply_function(fun=mne.baseline.rescale, times=times, baseline=(-0.1, 0), mode='mean')
+    #data = epochs.get_data()
+    #raw_array = mne.baseline.rescale(raw,times=times, baseline = (-0.1,0), mode='mean', copy=False)  
+    
+    #raw_array = mne.time_frequency.tfr_array_morlet(raw.get_data(), sfreq=128,freqs=freqs, n_cycles=freqs/2,output='avg_power')
+    # Baseline the output
+    #mne.baseline.rescale(raw_array, raw.times, (-0.1, 1), mode='mean', copy=False)
+
+    data_As += raw[Aud_event[0]].average().apply_baseline(baseline).get_data(picks=chan)[0]+raw[Aud_event[1]].average().apply_baseline(baseline).get_data(picks=chan)[0]
+    data_Vs += raw[Vis_event[0]].average().apply_baseline(baseline).get_data(picks=chan)[0]+raw[Vis_event[1]].average().apply_baseline(baseline).get_data(picks=chan)[0]
+    data_AVcs +=raw[AV_c[0]].average().apply_baseline(baseline).get_data(picks=chan)[0]+raw[AV_c[1]].average().apply_baseline(baseline).get_data(picks=chan)[0]
+    data_AVics +=raw[AV_ic[0]].average().apply_baseline(baseline).get_data(picks=chan)[0]+raw[AV_ic[1]].average().apply_baseline(baseline).get_data(picks=chan)[0]
+    #data_V.append(raw[Vis_event[0]].average().get_data(picks=chan)[0]+raw[Vis_event[1]].average().get_data(picks=chan)[0])
+    #data_AVc.append(raw[AV_c[0]].average().get_data(picks=chan)[0]+raw[AV_c[1]].average().get_data(picks=chan)[0])
+    #data_AVic.append(raw[AV_ic[0]].average().get_data(picks=chan)[0]+raw[AV_ic[1]].average().get_data(picks=chan)[0])
+data_As =data_As/len(Speech_files)
+data_Vs=data_Vs/len(Speech_files)
+data_AVcs =data_AVcs/len(Speech_files)-data_Vs
+data_AVics=data_AVics/len(Speech_files)-data_Vs
+all_datas = np.concatenate((data_As,data_Vs,data_AVics,data_AVcs))
+#mne.baseline.rescale(all_datas.get_data(),times=times,baseline=(-0.1,0), mode='mean', copy=False)
+
+#data from non-speech
+for file in Non_speech_files:
+    path = "/Users/ceciliehvilsted/Documents/GitHub/Fagpakkeprojekt2023_EEG/EEGproj-main/EEGproj-main/data_preproc/ " + str(file)
+    path = path.replace(" ","")
+    raw = mne.io.read_epochs_eeglab(path, montage_units='dm')
+    raw = raw.crop(tmin=-0.1)
+    #raw.apply_baseline(interval) #one baseline method
+    #raw_array = mne.baseline.rescale(raw,times=times, baseline = (-0.1,0), mode='mean', copy=False)
+    data_Ans += raw[Aud_event[0]].average().apply_baseline(baseline).get_data(picks=chan)[0]+raw[Aud_event[1]].average().apply_baseline(baseline).get_data(picks=chan)[0]
+    data_Vns += raw[Vis_event[0]].average().apply_baseline(baseline).get_data(picks=chan)[0]+raw[Vis_event[1]].average().apply_baseline(baseline).get_data(picks=chan)[0]
+    data_AVcns +=raw[AV_c[0]].average().apply_baseline(baseline).get_data(picks=chan)[0]+raw[AV_c[1]].average().apply_baseline(baseline).get_data(picks=chan)[0]
+    data_AVicns +=raw[AV_ic[0]].average().apply_baseline(baseline).get_data(picks=chan)[0]+raw[AV_ic[1]].average().apply_baseline(baseline).get_data(picks=chan)[0]
+    #data_V.append(raw[Vis_event[0]].average().get_data(picks=chan)[0]+raw[Vis_event[1]].average().get_data(picks=chan)[0])
+    #data_AVc.append(raw[AV_c[0]].average().get_data(picks=chan)[0]+raw[AV_c[1]].average().get_data(picks=chan)[0])
+    #data_AVic.append(raw[AV_ic[0]].average().get_data(picks=chan)[0]+raw[AV_ic[1]].average().get_data(picks=chan)[0])
+
+data_Ans =data_Ans/len(Non_speech_files)
+data_Vns=data_Vns/len(Non_speech_files)
+data_AVcns =data_AVcns/len(Non_speech_files)-data_Vns
+data_AVicns=data_AVicns/len(Non_speech_files)-data_Vns
+all_datans = np.concatenate((data_Ans,data_Vns,data_AVicns,data_AVcns))
+#mne.baseline.rescale(all_datans.get_data(),times=times,baseline=(-0.1,0), mode='mean', copy=False)
+
+'''
+x = np.arange(-0.1,1,step=1/128)
+fig, (ax1,ax2) = plt.subplots(1,2)
+ax1.axvline(x=0.1,color='r')
+ax1.plot(x,data_As,color='k', label='A')
+ax1.plot(x,data_AVics,color='k',linestyle='dashed', label='Incongruent AV')
+ax1.plot(x,data_AVcs,color='0.8', label = 'Congruent AV')
+ax1.set_yticks(np.arange(-6e-6,8e-6,2e-6))
+ax1.invert_yaxis()
+ax1.set_title('Speech')
+ax2.axvline(x=0.1,color='r')
+ax2.plot(x,data_Ans,color='k', label='A')
+ax2.plot(x,data_AVicns,color='k',linestyle='dashed', label='Incongruent AV')
+ax2.plot(x,data_AVcns,color='0.8', label = 'Congruent AV')
+ax2.set_yticks(np.arange(-6e-6,8e-6,2e-6))
+ax2.invert_yaxis()
+ax2.set_title('Non-speech')
+ax1.legend(loc='upper right', fontsize = 8)
+ax2.legend(loc='upper right', fontsize = 8)
+plt.show()
+'''
---
+++ t_testing_cec.py
@@ -0,0 +1,32 @@
+
+import scipy
+from data_load_cec import data_As, data_Ans, data_AVcns, data_AVcs, data_AVicns, data_AVics, data_Vns, data_Vs, all_datans, all_datas
+import mne
+import numpy as np
+
+# using scipy: t-test used compares the means of two independent samples of scores
+# speech
+test_As_in = scipy.stats.ttest_ind(data_As, data_AVics) # auditiv + incongruent
+test_As_con = scipy.stats.ttest_ind(data_As, data_AVcs) # auditiv + congruent
+#print('T-test for A and incongruent AV in speech mode gives: ' + str(test_As_in))
+print('T-test for A and congruent AV in speech mode gives: ' + str(test_As_con))
+
+# non-speech
+test_Ans_in = scipy.stats.ttest_ind(data_Ans, data_AVicns) # auditiv + incongruent
+test_Ans_con = scipy.stats.ttest_ind(data_Ans, data_AVcns) # auditiv + congruent
+#print('T-test for A and incongruent AV in non-speech mode gives: ' + str(test_Ans_in))
+#print('T-test for A and congruent AV in non-speech mode gives: ' + str(test_Ans_con))
+
+# using mne
+n_permutations = 50000
+T0, p_values, H0 = mne.stats.permutation_t_test(np.ndarray(data_As, all_datas), n_permutations, n_jobs=None)
+print(p_values)
+
+#significant_sensors = picks[p_values <= 0.05]
+#significant_sensors_names = [raw.ch_names[k] for k in significant_sensors]
+
+#print("Number of significant sensors : %d" % len(significant_sensors))
+#print("Sensors names : %s" % significant_sensors_names)
+# speech
+#speech_ttest = mne.stats.permutation_t_test(np.ndarray(all_datas, 4), n_permutations=10000, n_jobs=None)
+#print(speech_ttest)
