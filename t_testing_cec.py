7bd455f905efd565ae9e82fdcad23b9ed0b2f786
cdhvilsted <94805251+cdhvilsted@users.noreply.github.com>
2023-03-15 16:55:18 +0100

    Trying to fix t-test

--- t_testing_cec.py
+++ t_testing_cec.py
@@ -5,6 +5,8 @@ import mne
 import numpy as np
 
 # using scipy: t-test used compares the means of two independent samples of scores
+# N component
+times = np.arange(-0.1,1,step=1/128)
 # speech
 test_As_in = scipy.stats.ttest_ind(data_As, data_AVics) # auditiv + incongruent
 test_As_con = scipy.stats.ttest_ind(data_As, data_AVcs) # auditiv + congruent
@@ -18,9 +20,9 @@ test_Ans_con = scipy.stats.ttest_ind(data_Ans, data_AVcns) # auditiv + congruent
 #print('T-test for A and congruent AV in non-speech mode gives: ' + str(test_Ans_con))
 
 # using mne
-n_permutations = 50000
-T0, p_values, H0 = mne.stats.permutation_t_test(np.ndarray(data_As, all_datas), n_permutations, n_jobs=None)
-print(p_values)
+#n_permutations = 50000
+#T0, p_values, H0 = mne.stats.permutation_t_test(np.ndarray(data_As, all_datas), n_permutations, n_jobs=None)
+#print(p_values)
 
 #significant_sensors = picks[p_values <= 0.05]
 #significant_sensors_names = [raw.ch_names[k] for k in significant_sensors]
