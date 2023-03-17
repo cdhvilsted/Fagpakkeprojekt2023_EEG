fcae454ed537b4c273ced7d55c335ee3465b5261
Idlura <ida@raagart.dk>
2023-03-17 18:45:07 +0100

    just initializing by loading packages

--- ICA.py
+++ ICA.py
@@ -9,7 +9,7 @@ randomSeed = random.seed(10)
 epochs = mne.read_epochs_eeglab(path, montage_units='dm')
 
 # Apply ICA to the Epochs object
-ica = mne.preprocessing.ICA(n_components=20, method='fastica')
+ica = mne.preprocessing.ICA(n_components=28, method='fastica')
 ica.fit(epochs)
 
 # Plot the topographic maps of the independent components
